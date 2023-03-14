f8143159744d093bc4ae1a210f5eb47d9822948e
Wolfram-180 <sergei.ulvis@gmail.com>
2023-03-14 20:03:58 +0300

    upd

--- lib/main.dart
+++ lib/main.dart
@@ -24,13 +24,20 @@ class CustomFilePicker extends StatefulWidget {
 }
 
 class _CustomFilePicker extends State<CustomFilePicker> {
-  File? selectedfile = null;
+  File? selectedfile;
   late Response response;
   String progress = '';
+
   Dio dio = new Dio();
 
   selectFile() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles();
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: [
+        'jpg',
+        'jpeg',
+      ],
+    );
 
     if (result != null) {
       selectedfile = File(result.files.single.path!);
@@ -103,64 +110,70 @@ class _CustomFilePicker extends State<CustomFilePicker> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Select File and Upload"),
-          backgroundColor: Colors.orangeAccent,
-        ), //set appbar
-        body: Container(
-            alignment: Alignment.center,
-            padding: EdgeInsets.all(40),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.all(10),
-                  //show file name here
-                  child: progress == null
-                      ? Text("Progress: 0%")
-                      : Text(
-                          basename("Progress: $progress"),
-                          textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 18),
-                        ),
-                  //show progress status here
-                ),
+      appBar: AppBar(
+        title: Text("Select File and Upload"),
+        backgroundColor: Colors.orangeAccent,
+      ), //set appbar
+      body: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(40),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.all(10),
+              //show file name here
+              child: progress == null
+                  ? Text("Progress: 0%")
+                  : Text(
+                      basename("Progress: $progress"),
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 18),
+                    ),
+              //show progress status here
+            ),
 
-                Container(
-                  margin: EdgeInsets.all(10),
-                  //show file name here
-                  child: selectedfile == null
-                      ? Text("Choose File")
-                      : Text(basename(selectedfile!.path)),
-                  //basename is from path package, to get filename from path
-                  //check if file is selected, if yes then show file name
-                ),
+            Container(
+              margin: EdgeInsets.all(10),
+              //show file name here
+              child: selectedfile == null
+                  ? Text("Choose File")
+                  : Text(
+                      basename(selectedfile!.path),
+                    ),
+              //basename is from path package, to get filename from path
+              //check if file is selected, if yes then show file name
+            ),
 
-                Container(
-                    child: TextButton.icon(
-                  onPressed: () {
-                    selectFile();
-                  },
-                  icon: Icon(Icons.folder_open),
-                  label: Text("CHOOSE FILE"),
-                  //color: Colors.redAccent,
-                  //colorBrightness: Brightness.dark,
-                )),
+            Container(
+              child: TextButton.icon(
+                onPressed: () {
+                  selectFile();
+                },
+                icon: Icon(Icons.folder_open),
+                label: Text("CHOOSE FILE"),
+                //color: Colors.redAccent,
+                //colorBrightness: Brightness.dark,
+              ),
+            ),
 
-                //if selectedfile is null then show empty container
-                //if file is selected then show upload button
-                selectedfile == null
-                    ? Container()
-                    : Container(
-                        child: ElevatedButton.icon(
-                        onPressed: () {
-                          uploadFile();
-                        },
-                        icon: Icon(Icons.folder_open),
-                        label: Text("UPLOAD FILE"),
-                        //color: Colors.redAccent,
-                        //colorBrightness: Brightness.dark,
-                      ))
-              ],
-            )));
+            //if selectedfile is null then show empty container
+            //if file is selected then show upload button
+            selectedfile == null
+                ? Container()
+                : Container(
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        uploadFile();
+                      },
+                      icon: Icon(Icons.folder_open),
+                      label: Text("UPLOAD FILE"),
+                      //color: Colors.redAccent,
+                      //colorBrightness: Brightness.dark,
+                    ),
+                  )
+          ],
+        ),
+      ),
+    );
   }
 }
